694937b407752fbe28194b2a99c73816087c1943
Travis Martin <travisemerymartin@gmail.com>
2019-02-27 21:30:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,4 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  
+  if phrase == 'I LOVE YOU GRANDMA!'
+    return 'I LOVE YOU TOO PUMPKIN!'
+  elsif phrase == phrase.uppercase
+    return 'NO, NOT SINCE 1983!'
+  else
+    return 'HUH?! SPEAK UP, SONNY!'
+  end
+end
